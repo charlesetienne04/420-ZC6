fbb1d18ee2ab195e42e0dc2f392086165262b797
Charles-etienne Lacombe <charlesetienne2008@hotmail.com>
2025-11-05 15:02:30 -0500

    eeee

--- lacombech/Jour1/Probleme6.java
+++ lacombech/Jour1/Probleme6.java
@@ -6,54 +6,48 @@ public class Probleme6 {
     public static void main(String[] args) {
         String mtemp = "Quelle est votre nom?";
         String nom;
-        int note, note2 = 0;
+        int type, longueur, largeur;
         Scanner lectureClavier = new Scanner(System.in);
         afficherResultat(mtemp);
         nom = saisirNom2(lectureClavier);
         mtemp = "Quel est le type de plancher";
         afficherResultat(mtemp);
-        
-        /*for(int i = 1; i < 3;) {
-            if (i == 1) {
-                afficherResultat(mtemp);
-                i = 2;
-            }
-            note = saisirNom(lectureClavier);
-            if (note < 0 || note > 100) {
-                mtemp = "Le nombre est invalide";
-                afficherResultat(mtemp);
-            } else {
-                i = 3;
-                note2 = note;
-            }
-        } */
+        type = fortype(lectureClavier);
+        mtemp = "Quel est la longueur";
+        afficherResultat(mtemp);
+        longueur = longLarg(lectureClavier);
+        mtemp = "Quel est la largeur";
+        afficherResultat(mtemp);
+        largeur = longLarg(lectureClavier);
         lectureClavier.close();
         
         
-        notepass(note2);
+        
         
     }
-    public static int fortype(int type, Scanner lectureClavier) {
-        String mtemp = "Nombre invalide";
+    public static int fortype(Scanner lectureClavier) {
+        int type = 0;
+        String mtemp = "Vous n’avez pas choisit le bon type de plancher";
         for(int i = 1; i < 3;) {
             type = saisirNom(lectureClavier);
             if (type < 1 || type > 3) {
                 afficherResultat(mtemp);
-                i = 2;
             }
-            if (note < 0 || note > 100) {
-                mtemp = "Le nombre est invalide";
-                afficherResultat(mtemp);
-            } else {
+            else if (type > 0 || type < 4) {
                 i = 3;
-                note2 = note;
-            }
-        } 
+        }
+    } 
+        return type;
     }
     public static void afficherResultat(String message) {
         System.out.println(message);
     }
 
+    public static int longLarg(Scanner lecture) {
+        int chiffre;
+        chiffre = lecture.nextInt();
+        return chiffre;
+    }
     public static int saisirNom(Scanner lecture) {
         int chiffre;
         chiffre = lecture.nextInt();
@@ -63,16 +57,5 @@ public class Probleme6 {
         String chiffre;
         chiffre = lecture.nextLine();
         return chiffre;
-    }
-
-    public static void notepass(int notes) {
-        String a;
-        if (notes < 60) {
-            a = "Échec";
-            afficherResultat(a);
-        } else if (notes >= 60) {
-            a = "Réussite";
-            afficherResultat(a);
-        }
     }
 }
