d9009043ab919939ae528ebda58136ca92b7acb5
Charles-etienne Lacombe <charlesetienne2008@hotmail.com>
2025-11-06 11:53:59 -0500

    the

--- lacombech/Jour2/Probleme8d2.java
+++ lacombech/Jour2/Probleme8d2.java
@@ -1,7 +1,19 @@
 package lacombech.Jour2;
 
+import java.util.Scanner;
+
 public class Probleme8d2 {
     public static void main(String[] args) {
-    
+    String mot1, mot2, mottemp;
+    Scanner lectureClavier = new Scanner(System.in);
+        /* idée rn est utilisé les valeur char en int mais pas certain */
+
+
+
+    }
+    public static String saisirNom(Scanner lecture) {
+        String chiffre;
+        chiffre = lecture.nextLine();
+        return chiffre;
     }
 }
